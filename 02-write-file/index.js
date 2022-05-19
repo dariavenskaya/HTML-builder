f694495e60a0f6af509aa3e4f49b327f483aed33
dariavenskaya <venskayadaria@yandex.by>
2022-05-19 15:25:15 +0300

    feat: solve 02-write-fle

---
+++ 02-write-file/index.js
@@ -0,0 +1,24 @@
+let fs = require('fs');
+function openFile(){
+  fs.open('02-write-file/text.txt', 'a+', (err) => {
+    if(err) throw err;
+  });}
+openFile();
+let readline = require('readline'); 
+let rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: '>'
+});
+rl.prompt();
+rl.on('line', (input) => {
+  input = input.toLowerCase();
+  console.log(input);
+  rl.close();
+  fs.appendFile('02-write-file/text.txt', input, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+  });
+});
