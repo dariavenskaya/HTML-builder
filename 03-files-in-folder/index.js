78999c6edf6a0a8e0b7e63a696669e663319781e
dariavenskaya <venskayadaria@yandex.by>
2022-05-23 12:36:44 +0300

    feat: solve 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,9 +1,20 @@
 const fs = require('fs');
+const path = require('path');
+
 fs.readdir('03-files-in-folder/secret-folder', (err, files) => {
   if(err) throw err; 
   else {
     files.forEach(file => {
-      console.log(file);
+      fs.stat(path.join(__dirname, 'secret-folder', file), (err, stats) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        else {
+          let fileName = path.basename(file).toString().split('.')[0];
+          let fileExt = path.extname(file).toString().split('.')[1];
+          console.log(fileName, '-', fileExt, '-', stats.size / 1000 + 'kb');    
+        }});
     });
   }
 });
