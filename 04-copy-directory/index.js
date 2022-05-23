954db0b81fac32a08d79b13f02bb147b7d5f3f72
dariavenskaya <venskayadaria@yandex.by>
2022-05-24 00:06:14 +0300

    feat: half of the 3rd task is done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,19 +1,45 @@
 
-// const util = require('util');
+const util = require('util');
 const fs = require('fs');
-// const path = require('path');
-// const copyFilePromise = util.promisify(fs.copyFiles);
-// const srcDir = '04-copy-directory/files';
-// const destDir = '04-copy-directory/files-copy';
-fs.mkdir('04-copy-directory/files-copy', err => {
-  if(err) throw err;
+const path = require('path');
+const copyFilePromise = util.promisify(fs.copyFiles);
+const destDir = '04-copy-directory/files-copy';
+
+fs.mkdir(destDir, err => {
+  if(err) {
+    console.log('directory already exists');
+  }
   else{
-    console.log('done');
+    console.log('directory copied');
   }
 });
-fs.readdir('04-copy-directory/files-copy', (err, files) => {
-  if(err) throw err; 
-  else{
-    console.log(files);
-  }
-});
+function copyFiles(destDir, files) {
+  return Promise.all(files.map(f => {
+    const srcDir = '04-copy-directory/files';
+    return copyFilePromise(path.join(srcDir, f), path.join(destDir, f));
+  }));
+}
+copyFiles();
+// fs.readdir(srcDir, (err) => {
+//   if(err){
+//     console.log(err);
+//   } 
+//   else{
+//     fs.copyFiles(srcDir, destDir, files => {
+//       Promise.all(files.map(file => {
+//         copyFilePromise(path.join(srcDir, file), path.join(destDir, file));
+//       }));
+//     });
+    
+// files.forEach(file => {
+//   fs.copyFile('04-copy-directory/files'+file, '04-copy-directory/files-copy'+file, (err) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     else {
+//       console.log('File successfully copied and moved to new directory.');    
+//     }
+//   });
+// });
+//   }
+// });
--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,6 +1,32 @@
-module.exports = {
-  ...require('./test-files/styles/style_four.css'),
-  ...require('./test-files/styles/style_one.css'),
-  ...require('./test-files/styles/style_three.css'),
-  ...require('./test-files/styles/style_two.css')
-};
+let fs = require('fs');
+let path = require('path');
+let filesArr = [];
+fs.readdir('05-merge-styles/styles', (err, files) => {
+  if(err) throw err; 
+  else {
+    files.forEach(mergeFile => {
+      fs.readFile(path.join(__dirname, 'styles', mergeFile), (err) => {
+        if (err) throw err;
+        if(path.extname(mergeFile) == '.css'){
+          filesArr.push(mergeFile);
+          console.log(fs.readFile());
+          fs.writeFile(path.join(__dirname, 'project-dist', 'bundle.css'), '', (err) => {
+            if (err){
+              console.log('file is already here!');
+            }
+            else{
+              fs.appendFile(path.join(__dirname, 'project-dist', 'bundle.css'),  filesArr.reduce( (acc, el) => acc += el,), (err) => {
+                if (err) {
+                  console.log(err);
+                }
+                else{
+                  console.log('done, check your index.html!');
+                }
+              });
+            }
+          });
+          
+        }
+      });
+    });
+  }});
